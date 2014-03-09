89a4f74736d7d570021c1b001a3d492fa2627bf2
whhsw <whh.reg@gmail.com>
2014-03-08 21:52:21 -0800

    Update sudoku_solver.java

--- hand_writing/sudoku_solver.java
+++ hand_writing/sudoku_solver.java
@@ -10,14 +10,14 @@ public class Solution {
     }
 
     public boolean solve(char[][] board) {
-        for(int i=0; i < 9; i++) {
-            for(int j=0; j<9; j++){
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++){
                 if(board[i][j] != '.'){
                     continue;
                 }
-                for(int k=1; k<=9; k++){
+                for(int k = 1; k <= 9; k++){
                     board[i][j] = (char) (k + '0');
-                    if(isValid(board, i, j) && solve(board)){
+                    if (isValid(board, i, j) && solve(board)){
                         return true;
                     }
                     board[i][j] = '.';
@@ -41,23 +41,28 @@ public class Solution {
     
         contained = new HashSet<Character>();
         for(int j=0;j<9;j++){
-            if(contained.contains(board[j][b])) return false;
-            if(board[j][b]>'0' && board[j][b]<='9')
+            if (contained.contains(board[j][b])) { 
+                return false;
+            }
+            if (board[j][b]>'0' && board[j][b]<='9') {
                 contained.add(board[j][b]);
+            }
         }
         
     
         contained = new HashSet<Character>();
-        for(int m=0;m<3;m++){
-            for(int n=0;n<3;n++){
-                int x=a/3*3+m, y=b/3*3+n;
-                if(contained.contains(board[x][y])) return false;
-                    if(board[x][y]>'0' && board[x][y]<='9')
+        for (int m = 0; m < 3; m++) {
+            for (int n = 0; n < 3; n++){
+                int x = a / 3 * 3 + m, y = b / 3 * 3 + n;
+                if (contained.contains(board[x][y])) {
+                    return false;
+                }
+                if (board[x][y] > '0' && board[x][y] <= '9') {
                         contained.add(board[x][y]);
+                }
             }  
         }
     
         return true;
     }
-    
 }
