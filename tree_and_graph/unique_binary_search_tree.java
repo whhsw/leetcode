b4da37c384ef803afbbef0a280589d1a2e553d5d
whhsw <whh.reg@gmail.com>
2014-03-08 21:05:33 -0800

    Update unique_binary_search_tree.java

--- tree_and_graph/unique_binary_search_tree.java
+++ tree_and_graph/unique_binary_search_tree.java
@@ -37,7 +37,7 @@ http://fisherlei.blogspot.com/2013/03/leetcode-unique-binary-search-trees.html
         
         for(int i=2;  i<= n; i++){
             for(int j=0; j<i; j++){
-                count[i] += count[j]*count[i-j-1];
+                count[i] += count[j] * count[i - j - 1];
             }
         }
         return count[n];
